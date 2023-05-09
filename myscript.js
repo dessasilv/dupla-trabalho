01c1e941d60028f8a8b174605b7cdc125b38cf4c
analluiza <analluizafr@gmail.com>
2023-05-09 09:22:26 -0300

    aula dia 09/05

---
+++ myscript.js
@@ -0,0 +1,6 @@
+function carregar(){
+    var msg  = window.document.getElementById("variavel") //colocar id da mensagem
+    var img = window.document.getElementById("imagens") //colocar id da img
+    msg.InnerHTML= 'modificando com js'
+    img.src= "img/Camada2.png"
+    }
