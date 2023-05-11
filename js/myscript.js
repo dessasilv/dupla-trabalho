3850143260bece3828aede4b7bd88f7b31ac6c3c
dessasilv <andressasilva1423ag@gmail.com>
2023-05-11 11:20:55 -0300

    .

---
+++ js/myscript.js
@@ -0,0 +1,9 @@
+function carregar(){
+    alert ("nosso site")
+    }
+    function myFunction(){
+        
+        document.getElementById("hoje").innerHTML = new Date()
+        hoje.getHours()
+        ;
+    }
--- myscript.js
+++ myscript.js
@@ -1,6 +1,9 @@
-function carregar(){
-    var msg  = window.document.getElementById("variavel") //colocar id da mensagem
-    var img = window.document.getElementById("imagens") //colocar id da img
-    msg.InnerHTML= 'modificando com js'
-    img.src= "img/Camada2.png"
-    }
+
+    
+    
+        function carregar(){
+            var msg  = window.document.getElementById("variavel") //colocar id da mensagem
+            var img = window.document.getElementById("imagens") //colocar id da img
+            msg.InnerHTML= 'modificando com js'
+            img.src= "img/Camada2.png"
+            }
