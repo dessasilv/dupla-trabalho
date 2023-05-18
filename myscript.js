4edbf1b648625f756b0d9280a9a6f908fc683452
dessasilv <130998888+dessasilv@users.noreply.github.com>
2023-05-18 08:02:36 -0300

    Delete myscript.js

--- myscript.js
+++
@@ -1,9 +0,0 @@
-
-    
-    
-        function carregar(){
-            var msg  = window.document.getElementById("variavel") //colocar id da mensagem
-            var img = window.document.getElementById("imagens") //colocar id da img
-            msg.InnerHTML= 'modificando com js'
-            img.src= "img/Camada2.png"
-            }
